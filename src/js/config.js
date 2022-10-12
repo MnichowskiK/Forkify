e27813df5f6c71a634fc392f7c463105e2627861
MnichowskiK <kamil.mnichowski@gmail.com>
2022-10-12 18:19:00 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5f915c79-5d99-4baf-9406-70e5f2a4f825';
+export const MODAL_CLOSE_SEC = 2.5;
